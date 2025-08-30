deec6f4be000e02532bb87f9ec0b6f1a03e2cce0
KurraSanz <msillanas@gmail.com>
2025-08-30 18:31:23 +0200

    Add files via upload

--- proceso_curado_eea.py
+++ proceso_curado_eea.py
@@ -14,7 +14,8 @@ Acciones que realiza el script:
    - En el archivo especial `ETS_Database_v51_May23.csv`, elimina filas con el valor "Total" en la columna `year`.
 3. Realiza la conversión de tipos:
    - Convierte columnas como `emissions`, `value`, `year` y `Year` a formatos numéricos (`float64`, `int64`).
-4. Guarda los archivos curados en la carpeta `ficheros_curado/eea`, manteniendo el nombre original.
+4. Eliminar registros duplicados.
+5. Guarda los archivos curados en la carpeta `ficheros_curado/eea`, manteniendo el nombre original.
 
 '''
 
@@ -64,7 +65,9 @@ for archivo in tqdm(archivos_raw, desc="Limpiando y guardando archivos raw"):
             df['value'] = pd.to_numeric(df['value'], errors='coerce').astype('float64')
         if 'year' in df.columns:
             df['year'] = pd.to_numeric(df['year'], errors='coerce').astype('int64')
-            
+
+        # 3) Eliminar registros duplicados
+        df = df.drop_duplicates()   
         # Guardar CSV curado
         nombre = os.path.basename(archivo)
         ruta_curado = os.path.join(CARPETA_CURADO, nombre)
